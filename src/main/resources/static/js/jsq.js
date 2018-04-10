02c119ffc850f5ebfa56edda503ba073d5b69d76
WANDLT <13693943814@163.com>
2018-04-11 02:07:22 +0800

    设备管理日历插件

--- src/main/resources/static/js/jsq.js
+++ src/main/resources/static/js/jsq.js
@@ -7,8 +7,8 @@ $(function(){
 					//清除时间
 					window.clearInterval(clearTime2);
 					var $getTimeIpt1=$("#getTimeIpt_1"),
-						$getTimeIpt2=$("#getTimeIpt_1"),
-						$getTimeIpt3=$("#getTimeIpt_3"),
+						$modifystart=$("#modifystart"),
+						$modifyend=$("#modifyend"),
 						$getTimeIpt4=$("#getTimeIpt_4"),
 						$getTimeIpt5=$("#getTimeIpt_5"),
 						$getTimeIpt6=$("#getTimeIpt_6");
@@ -17,11 +17,11 @@ $(function(){
 					//系统时间
 					data2.nowdate=$getTimeIpt1.val();
 					//开始时间
-					data2.startdate=$getTimeIpt2.val();
+					data2.startdate=$modifystart.val();
 					//结束时间
-					data2.enddate=$getTimeIpt3.val();;
+					data2.enddate=$modifyend.val();;
 					//是否跳过开始
-					data2.init=$getTimeIpt5.find("option:selected").val()=="false"?false:true;
+					data2.init=$getTimeIpt5.find("option:selected").val()===false;
 					clearTime2=$.leftTime(data2,function(d){
 					if(d.status){
 						var $dateShow1=$("#dateShow3");
@@ -29,10 +29,20 @@ $(function(){
 						$dateShow1.find(".h").html(d.h);
 						$dateShow1.find(".m").html(d.m);
 						$dateShow1.find(".s").html(d.s);
-						
-
-
-						
+						switch(d.step){
+							case 1:
+							$("#dataInfoShow_2").html("距离开始时间：");
+							break;
+							case 2:
+							$("#dataInfoShow_2").html("距离结束时间：");
+							break;
+							case 3:
+							$("#dataInfoShow_2").html("倒计时已结束：");
+							break;
+							default: 
+							$("#dataInfoShow_2").html("");
+							 break;
+						}
 					}
 				},$getTimeIpt6.find("option:selected").val()=="false"?false:true);
 				}
@@ -41,3 +51,57 @@ $(function(){
 				//手动触发
 				$("#dateBtn3").on("click",setDateImportFn);
 			});
+
+//日期范围限制
+
+var start = {
+    elem: '#start',
+    format: 'YYYY-MM-DD',
+    // min: laydate.now(), //设定最小日期为当前日期
+    max: '2099-06-16', //最大日期
+    istime: true,
+    istoday: false,
+    // choose: function(datas){
+    //      end.min = datas; //开始日选好后，重置结束日的最小日期
+    //      end.start = datas //将结束日的初始值设定为开始日
+    // }
+};
+var end = {
+    elem: '#end',
+    format: 'YYYY-MM-DD',
+    // min: laydate.now(),
+    max: '2099-06-16',
+    istime: true,
+    istoday: false,
+    // choose: function(datas){
+    //     start.max = datas; //结束日选好后，充值开始日的最大日期
+    // }
+};
+var modifystart = {
+    elem: '#modifystart',
+    format: 'YYYY-MM-DD',
+    // min: laydate.now(),
+    max: '2099-06-16',
+    istime: true,
+    istoday: false,
+    // choose: function(datas){
+    //     start.max = datas; //结束日选好后，充值开始日的最大日期
+    // }
+};
+var modifyend = {
+    elem: '#modifyend',
+    format: 'YYYY-MM-DD',
+    // min: laydate.now(),
+    max: '2099-06-16',
+    istime: true,
+    istoday: false,
+    // choose: function(datas){
+    //     start.max = datas; //结束日选好后，充值开始日的最大日期
+    // }
+};
+laydate(start);
+laydate(end);
+laydate(modifystart);
+laydate(modifyend);
+
+
