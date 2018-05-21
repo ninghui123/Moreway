b40d9bbb83801fba7c8639011ab333d792477870
WANDLT <13693943814@163.com>
2018-05-21 11:35:46 +0800

    设备添加

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -11,14 +11,15 @@ function login() {
         dataType : "json",
         data:{"username":name,"password":pwd},
 
-        success:function (ReturnMsg) {
-            if (ReturnMsg.code===500){
-                toastr.error("用户名或密码有误，请从新输入!");
-            }
-            if (ReturnMsg.code===200){
-                window.location.href ="index.html"
-
-            }
+        success:function (success) {
+            alert(success.code);
+            // if (success.code===500){
+            //     toastr.error("用户名或密码有误，请从新输入!");
+            // }
+            // if (success.code===200){
+            //     // window.location.href = "user.html";
+            //
+            // }
         }
     })
 }
